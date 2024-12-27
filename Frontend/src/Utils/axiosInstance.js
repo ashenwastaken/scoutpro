8238cddbacced3a7526e93b0254da1934239b43c
Shayan <msfq2468@gmail.com>
2024-12-27 18:31:03 +0500

    yassu di ballay ballay3

--- Frontend/src/Utils/axiosInstance.js
+++ Frontend/src/Utils/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: process.env.REACT_APP_PUBLIC_URL,
     withCredentials: true,
 });
 
